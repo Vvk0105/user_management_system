9cb42853356e9d4a436b3dbc9d633d29c0abcf06
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-07 23:59:47 +0530

    requirement file edited

--- user_management/users/urls.py
+++ user_management/users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('user_register/', user_register, name='user_register'),
     path('', login_page, name='login_page'),
+    path('login-page/', login_page, name='login_page'),
     path('edit-profile/', profile_edit, name='edit_profile'),
     path('user-notes/', user_notes, name='user_notes'),
     path('edit-note/', edit_note, name='edit_note'),
