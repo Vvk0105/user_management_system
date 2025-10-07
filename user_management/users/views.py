130eb96d97bdf345b64d841fb5e9dfea2eeef064
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-07 18:59:12 +0530

    user registration login apis done

--- user_management/users/views.py
+++ user_management/users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, ProfileSerializer
+from .models import Profile
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
