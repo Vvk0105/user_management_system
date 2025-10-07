8fc34b0a561236453239c8d0287dd2bb109c5b67
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-08 00:12:14 +0530

    Add Procfile and gunicorn for Render

--- user_management/user_management/settings.py
+++ user_management/user_management/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', '') != 'False'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['user_management.onrender.com', '127.0.0.1']
-
 
 # Application definition
 
