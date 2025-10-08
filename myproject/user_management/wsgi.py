be6395c797da35b7f606e835e60956d6eb78da06
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-08 11:36:22 +0530

    add some settings

--- myproject/user_management/wsgi.py
+++ myproject/user_management/wsgi.py
@@ -1,20 +1,22 @@
-"""
-WSGI config for user_management project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-
-# Add the project root to Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_management.settings')
 
 application = get_wsgi_application()
+
+class CorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With, X-CSRFToken"
+        response["Access-Control-Allow-Credentials"] = "true"
+        
+        return response
+
+application = CorsMiddleware(application)
